a55fd1d98f86aff3e4f64bfdcab92cf965039027
catherinecunningham <cfunk512@gmail.com>
2022-08-11 21:07:55 -0700

    add Amazon search API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,51 @@
 var todayBtn = document.getElementById("today-Btn")
 var thisWeekBtn = document.getElementById("this-week-Btn")
 var thisMonthBtn = document.getElementById("this-month-Btn")
+var stuffSearchBtn = document.getElementById("search-stuff")
+var myKey = "4591EDE1B7CE49AEB4BDD4631503A1CC"
+var yelpKey = "dUJs6RmTFhXg_LpfFZdZ8g2ur2grHni6hJA0ucA8avz_358XpiAqaQ89lIbJ0eVRPmnalNz69RTk6pNYE5kgczF_8wEPlaUoqqCD0cZS_90GGXRGzHxCvrGg-s31YnYx"
+var myYelpClientID = "NYv4yWFnhs_jumHwLKxoIw"
 
 // FUNCTIONS
 
+// Today Button clicked and current date stored with task
+
+// This Week Button clicked and end-of-week date stored with task
+
+// This Month Button clicked and end-of-month date stored with task
+
 // EVENT LISTENERS
+
+
+// Amazon search API
+function searchStuff(e) {
+    e.preventDefault()
+    var inputStuff = document.getElementById("input-stuff")
+    
+    fetch ("https://api.rainforestapi.com/request?api_key="+myKey+"&type=search&amazon_domain=amazon.com&search_term="+inputStuff.value)
+    .then((res)=>res.json()) 
+    .then((data)=>{
+    console.log(data)
+    var getImageURL = data.search_results[0].image
+    var getLinkURL = data.search_results[0].link
+    var searchImage = document.getElementById("search-image")
+    var clickImage = document.getElementById("click-image")
+    searchImage.setAttribute("src", getImageURL)
+    clickImage.setAttribute("href", getLinkURL)
+})
+
+}
+
+// Yelp Business Search API
+function searchBusiness(e) {
+    e.preventDefault()
+
+    fetch ("https://api.rainforestapi.com/request?api_key="+myKey+"&type=search&amazon_domain=amazon.com&search_term="+inputStuff.value)
+    .then((res)=>res.json()) 
+    .then((data)=>{
+    console.log(data)
+
+    })
+}
+
+stuffSearchBtn.addEventListener('click', searchStuff)
