44a6525691cd7070d7123d72d92a0286a41caf40
catherinecunningham <cfunk512@gmail.com>
2022-08-11 18:51:26 -0700

    timeframe button DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// GLOBAL VARIABLES
+var todayBtn = document.getElementById("today-Btn")
+var thisWeekBtn = document.getElementById("this-week-Btn")
+var thisMonthBtn = document.getElementById("this-month-Btn")
+
+// FUNCTIONS
+
+// EVENT LISTENERS
